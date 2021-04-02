478e849c9b1dfbf1dbee030ce2dc87be1b5e605c
André Varandas <andre.m.varandas@gmail.com>
2021-04-02 22:51:42 +0100

    feat(casing): converts object keys to camelCase

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -1,3 +1,4 @@
+import { camelCase } from 'camel-case'
 import fs from 'fs'
 import https from 'https'
 import Papa, { ParseConfig, ParseError } from 'papaparse'
@@ -17,11 +18,21 @@ export class DataProvider {
     const file = fs.createReadStream(CSV_FILE_PATH)
     const papaConfig: ParseConfig<unknown> = {
       header: true,
-      complete: ({ data }) =>
+      complete: ({ data }) => {
+        // Convert data object from snake case to camel
+        data.forEach((element: any) => {
+          for (const [key, value] of Object.entries(element)) {
+            if (camelCase(key) !== key)
+              delete Object.assign(element, { [camelCase(key)]: value })[key]
+          }
+        })
+
+        // Write the new data to a JSON file
         fs.writeFile(JSON_FILE_PATH, JSON.stringify(data), (err) => {
           if (err) return console.error(err)
           console.log(`File wrote to ${JSON_FILE_PATH}`)
-        }),
+        })
+      },
       error: (error: ParseError) => console.error(error),
     }
 
